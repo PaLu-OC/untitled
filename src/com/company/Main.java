ac511c048c65b6ed542c6094cb96bcdbfe5b82c8
Patrick Lucas <patricklucas@OCS442-MacBook-Pro.local>
2021-09-30 17:12:37 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = (args.length > 0 ? " " + String.join(" ", args) : "");
+        System.out.println("Hello" + text + "!");
+    }
+}
