e2c9ea23d1d2aa656d699de48e8b34f2b92875cd
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2021-09-30 17:20:01 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String text = (args.length > 0 ? " " + String.join(" ", args) : "");
-        System.out.println("Hello" + text + "!");
+        System.out.println("Hello" + text);
     }
 }
